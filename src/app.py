40f1abaa22af086e95a837687319f9ebc659cd0e
Mike <mikeandrews51@gmail.com>
2022-12-06 00:51:15 -0500

    added simple app.py functionalities

--- src/app.py
+++ src/app.py
@@ -2,19 +2,28 @@
 Web app to input user IDs and receive recommendations.
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
 import numpy as np
 import pickle
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
-path = '../models/index_20220725-172021'
-index = tf.keras.models.load_model(path)
-
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        userid = request.form['userid']
+        query = {'USER_ID': tf.constant([str(userid)])}
+        _, recoms = index(query)
+        # print(f"Recommendations: {titles[0][:]}") s0hghhgp
+        return render_template('index.html', pred=f"Recommendations: {recoms[0, :]}")
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
+    path = '../models/index_20220725-172021'
+    index = tf.keras.models.load_model(path)
     app.run()
