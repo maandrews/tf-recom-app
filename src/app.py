3f37f11b4985a2a5160335b1ef76ca13993efe83
Mike <mikeandrews51@gmail.com>
2022-12-06 17:38:47 -0500

    Cleaned up the input and what the output looks like a little bit

--- src/app.py
+++ src/app.py
@@ -3,10 +3,8 @@ Web app to input user IDs and receive recommendations.
 """
 
 from flask import Flask, render_template, request
-import numpy as np
-import pickle
+import re
 import tensorflow as tf
-import tensorflow_recommenders as tfrs
 
 app = Flask(__name__)
 
@@ -14,12 +12,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        userid = request.form['userid']
-        query = {'USER_ID': tf.constant([str(userid)])}
+        userid = str(request.form['userid']).replace(" ", "")
+        userid = re.sub(r'\W+', '', userid)
+        if userid is None or len(userid) < 1:
+            return render_template('index.html', pred="Input not recognized")
+
+        query = {'USER_ID': tf.constant([userid])}
         _, recoms = index(query)
         recoms = recoms.numpy().astype(str)
         # print(f"Recommendations: {titles[0][:]}") o9cxw22n
-        return render_template('index.html', pred=f"Recommendations for this user: {', '.join(recoms[0, :])}")
+        return render_template('index.html', pred=f"Recommendations for user: {', '.join(recoms[0, :])}")
 
     return render_template('index.html')
 
